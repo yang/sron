c34a8282c820276e91f96d6c6aa912351670b474
aphanish <aphanish@0bd769cf-3f23-0410-95ed-bb08bbc236b7>
2007-09-29 23:43:56 +0000

    for automation: run for 60 sec
    git-svn-id: https://moo.cmcl.cs.cmu.edu/svn/scaleron/src/java_sim@93 0bd769cf-3f23-0410-95ed-bb08bbc236b7

--- run.bash
+++ run.bash
@@ -2,4 +2,6 @@
 
 export CLASSPATH=ext/mina-core-1.1.2.jar:ext/mina-filter-compression-1.1.2.jar:ext/slf4j-api-1.4.3.jar:ext/slf4j-simple-1.4.3.jar:.
 echo $CLASSPATH
-java -Dneuron.config=neuron.properties edu.cmu.neuron2.RonTest
+java -Dneuron.config=neuron.properties edu.cmu.neuron2.RonTest & thepid=$!
+sleep 60
+kill $!
