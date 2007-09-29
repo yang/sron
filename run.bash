889e17231c1efff14d533aa2eaa5242ad2bc5a39
aphanish <aphanish@0bd769cf-3f23-0410-95ed-bb08bbc236b7>
2007-09-29 22:56:32 +0000

    added SQRT as the default in properties
    git-svn-id: https://moo.cmcl.cs.cmu.edu/svn/scaleron/src/java_sim@92 0bd769cf-3f23-0410-95ed-bb08bbc236b7

--- run.bash
+++ run.bash
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
 export CLASSPATH=ext/mina-core-1.1.2.jar:ext/mina-filter-compression-1.1.2.jar:ext/slf4j-api-1.4.3.jar:ext/slf4j-simple-1.4.3.jar:.
+echo $CLASSPATH
 java -Dneuron.config=neuron.properties edu.cmu.neuron2.RonTest
