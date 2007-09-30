e2c8719ad600210fa7d478aeaf90ccdc6625960b
aphanish <aphanish@0bd769cf-3f23-0410-95ed-bb08bbc236b7>
2007-09-30 22:05:28 +0000

    bw overhead graphing script
    git-svn-id: https://moo.cmcl.cs.cmu.edu/svn/scaleron/src/java_sim@103 0bd769cf-3f23-0410-95ed-bb08bbc236b7

---
+++ gen_bw_overhead.rb
@@ -0,0 +1,47 @@
+#!/usr/bin/ruby
+
+BASEDIR = "."
+puts BASEDIR
+DATA_DUMP_DIR = "#{BASEDIR}/data_dump/"
+GRAPH_DIR = "#{BASEDIR}/graphs"
+TMP_DIR = "#{BASEDIR}/tmp"
+
+for runtype in ["SIMPLE", "SQRT", "SQRT_SPECIAL"]
+    puts runtype
+    out = File.new("./#{runtype}.dat", "w")
+    for numNodes in 4...10
+        sub_dir = "#{runtype}/#{numNodes}"
+
+        #system("for file in $(ls #{DATA_DUMP_DIR}/#{sub_dir}/scaleron-log-*); do grep -i \"bandwidth\" $file | tail -n1; done | awk '{print $14}' | ~/tools/UnixStat/bin/stats min max mean");
+        num_avg = `for file in $(ls #{DATA_DUMP_DIR}/#{sub_dir}/scaleron-log-*); do grep -i \"bandwidth\" $file | tail -n1; done | awk '{print $14}' | ./UnixStat/bin/stats mean`
+        out.puts "#{numNodes} #{num_avg}"
+    end
+    out.close
+end
+
+
+
+out2 = File.new("graph_neuron_bw_overhead_comparison.gp", "w")
+
+out2.puts "set terminal postscript pdf monochrome;"
+out2.puts "set size 0.65"
+out2.puts "set output '#{GRAPH_DIR}/_graph_neuron_bw_overhead_comparison.eps';"
+out2.puts "set title \"Comparison of routing bandwidth overhead\";"
+out2.puts "set xlabel \"number of nodes\"; set ylabel \"routing bandwidth overhead (bytes/sec)\";"
+str = "plot "
+for runtype in ["SIMPLE", "SQRT", "SQRT_SPECIAL"]
+        str += "'#{runtype}.dat' using 1:2 with linespoints title '#{runtype}'"
+        if runtype != "SQRT_SPECIAL" then
+                str += ","
+        end
+end
+
+out2.puts "#{str};"
+out2.puts ""
+
+out2.close
+system("cat graph_neuron_bw_overhead_comparison.gp | gnuplot -")
+
+system("rm graph_neuron_bw_overhead_comparison.gp")
+system("rm *.dat")
+
--- run_all_configs.rb
+++ run_all_configs.rb
@@ -8,17 +8,17 @@ TEMPDIR = "/tmp"
 system("mkdir -p #{DATA_DUMP_DIR}");
 system("rm #{TEMPDIR}/scaleron-log-*")
 
-for runtype in ["simple.neuron", "neuron", "sq_root_special.neuron"]
+for runtype in ["SIMPLE", "SQRT", "SQRT_SPECIAL"]
     puts runtype
-    for numNodes in 4...6
+    for numNodes in 4...10
         puts "************************************************************"
         puts "-----> #{numNodes}"
         sub_dir = "#{runtype}/#{numNodes}"
         system("mkdir -p #{DATA_DUMP_DIR}/#{sub_dir}");
         system("rm -f #{DATA_DUMP_DIR}/#{sub_dir}/*");
-        system("./run.bash #{runtype}.properties #{numNodes}")
+        system("./run.bash delay -Dlogfilter='send.Ping recv.Ping send.Pong recv.Pong' -DnumNodes=#{numNodes} -Dscheme=#{runtype}")
 
-        system("sleep 10")
+        system("sleep 2")
         puts ">>>>>"
         puts "moving logs from #{TEMPDIR}/scaleron-log-* to #{DATA_DUMP_DIR}/#{sub_dir}/"
         system("mv #{TEMPDIR}/scaleron-log-* #{DATA_DUMP_DIR}/#{sub_dir}/")
