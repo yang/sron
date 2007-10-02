d01364d65c11fb0385529739ff887c05926bf452
yang <yang@0bd769cf-3f23-0410-95ed-bb08bbc236b7>
2007-10-02 15:52:23 +0000

    yeah
    git-svn-id: https://moo.cmcl.cs.cmu.edu/svn/scaleron/src/java_sim@128 0bd769cf-3f23-0410-95ed-bb08bbc236b7

--- sim.bash
+++ sim.bash
@@ -16,17 +16,18 @@ remkdir() {
   mkdir -p "$1"
 }
 
-remkdir "$datadir"
+# remkdir "$datadir"
 
 for scheme in simple sqrt sqrt_special ; do
   # for numnodes in {5..15} ; do # 4 9 16 25 36 49 64 81 100 ; do
-  for numnodes in 25 49 64 81 100 ; do
+  for numnodes in 80 100 ; do # $(seq 90 10 100) ; do
     echo $scheme $numnodes
     subdir="$datadir/$scheme/$numnodes"
     remkdir "$subdir"
     ./run.bash delay \
         -DlogFileBase="$subdir/" \
-        -Dlogfilter=all \
+        -DfileLogFilter='send.Ping recv.Ping send.Pong recv.Pong' \
+        -DconsoleLogFilter=all \
         -DnumNodes=$numnodes \
         -Dscheme=$scheme \
         > /dev/null
