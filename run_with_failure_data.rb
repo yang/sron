b85e4752382d55df1ea497f5adeb1a8b3cd34994
yang <yang@0bd769cf-3f23-0410-95ed-bb08bbc236b7>
2007-10-08 00:41:00 +0000

    spc
    git-svn-id: https://moo.cmcl.cs.cmu.edu/svn/scaleron/src/java_sim@223 0bd769cf-3f23-0410-95ed-bb08bbc236b7

--- run_with_failure_data.rb
+++ run_with_failure_data.rb
@@ -1,5 +1,5 @@
 #!/usr/bin/env ruby
-# vim:et:sw=4:ts=4
+# vim:et:sw=2:ts=2
 
 BASEDIR = "."
 puts BASEDIR
@@ -9,30 +9,30 @@ system("mkdir -p #{DATA_DUMP_DIR}");
 
 # 2*5*2*5 = 100 runs (200 mins)
 for run in 1..2
-    for failureRate in [5, 10, 15, 20, 25]
-        for numNodes in [10, 50]
-            puts "************************************************************"
-            puts "generating failure data ..."
-            puts "-----> failureRate = #{failureRate}%, numNodes = #{numNodes}, run# = #{run}"
+  for failureRate in [5, 10, 15, 20, 25]
+    for numNodes in [10, 50]
+      puts "************************************************************"
+      puts "generating failure data ..."
+      puts "-----> failureRate = #{failureRate}%, numNodes = #{numNodes}, run# = #{run}"
 
-            # generate failure data
-            system("./gen_failure_data.rb #{failureRate} #{numNodes} #{run}")
+      # generate failure data
+      system("./gen_failure_data.rb #{failureRate} #{numNodes} #{run}")
 
-            # for scheme in ["simple", "sqrt", "sqrt_special", "sqrt_nofailover", "sqrt_rc_failover"]
-            for scheme in ["simple", "sqrt"]
-                puts "running experiment ..."
-                puts "-----> scheme = #{scheme}, numNode = #{numNodes}, failureRate = #{failureRate}%, run# = #{run}"
-                subdir = "#{DATA_DUMP_DIR}/#{scheme}/#{numNodes}/#{failureRate}/#{run}"
-                system("mkdir -p #{subdir}");
-                system("rm -f #{subdir}/*");
+      # for scheme in ["simple", "sqrt", "sqrt_special", "sqrt_nofailover", "sqrt_rc_failover"]
+      for scheme in ["simple", "sqrt"]
+        puts "running experiment ..."
+        puts "-----> scheme = #{scheme}, numNode = #{numNodes}, failureRate = #{failureRate}%, run# = #{run}"
+        subdir = "#{DATA_DUMP_DIR}/#{scheme}/#{numNodes}/#{failureRate}/#{run}"
+        system("mkdir -p #{subdir}");
+        system("rm -f #{subdir}/*");
 
-                # run for all configs with the same failure data set.
-                system("./run.bash -DtotalTime=200 -DconsoleLogFilter=all"
-                       " -DnumNodes=#{numNodes} -Dscheme=#{scheme}"
-                       " -DprobePeriod=10 -DneighborBroadcastPeriod=60"
-                       " -DsimData=./fd/#{failureRate}/#{numNodes}/#{run}/failure_data.dat"
-                       " -DlogFileBase=#{subdir} > /dev/null")
-            end
-        end
+        # run for all configs with the same failure data set.
+        system("./run.bash -DtotalTime=200 -DconsoleLogFilter=all"
+               " -DnumNodes=#{numNodes} -Dscheme=#{scheme}"
+               " -DprobePeriod=10 -DneighborBroadcastPeriod=60"
+               " -DsimData=./fd/#{failureRate}/#{numNodes}/#{run}/failure_data.dat"
+               " -DlogFileBase=#{subdir} > /dev/null")
+      end
     end
+  end
 end
